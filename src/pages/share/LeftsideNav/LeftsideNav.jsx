1fb504bf807c4c662544f688288749e21668baa5
khairul-islam98 <janeyrahman900@gmail.com>
2023-10-05 16:29:26 +0600

    create categories file in the fields

--- src/pages/share/LeftsideNav/LeftsideNav.jsx
+++ src/pages/share/LeftsideNav/LeftsideNav.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftsideNav = () => {
+    const [categories, setCategories] = useState([])
+    useEffect(() => {
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    }, [])
     return (
-        <div>
-            <h2 className="text-2xl">Left side nav</h2>
+        <div className='space-y-6'>
+            <h2 className="text-2xl">All Categories</h2>
+            {
+                categories.map(category => <Link to={`/category/${category.id}`} className='block ml-4 text-xl font-semibold'>{category.name}</Link>)
+            }
         </div>
     );
 };
